71b44ac73820ab5a8355aedceb1793bc6d93a05a
Hiếu Nguyễn <nguyenhotrunghieu0106@gmail.com>
2022-11-21 17:26:27 +0700

    Change api to 3rd party, now permanent

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -5,16 +5,19 @@ import "./Content.css";
 function Content() {
   const data = useSelector(state => state.search.data);
   const renderData = data.map((item, index) => {
+    const album = item.data;
+    const uri = "https://open.spotify.com/album/" + album.uri.slice(14);
+
     return (
       <li key={index} className="content__item col">
-        <a href={item.external_urls.spotify} target="_blank" rel="noreferrer">
+        <a href={uri} target="_blank" rel="noreferrer">
           <div className="content__image">
-            <img src={item.images[0].url} alt="Album Cover" />
+            <img src={album.coverArt.sources[0].url} alt="Album Cover" />
           </div>
           <div className="content__info">
-            <h3 className="content__title">{item.name}</h3>
-            <p className="content__release-date">Release date: {item.release_date}</p>
-            <p className="content__tracks-count">Number of tracks: {item.total_tracks}</p>
+            <h3 className="content__title">{album.name}</h3>
+            <p className="content__release-date">Release year: {album.date.year}</p>
+            {/* <p className="content__tracks-count">Number of tracks: {item.total_tracks}</p> */}
           </div>
         </a>
       </li>
--- src/components/Header/SearchBar/SearchBar.js
+++ src/components/Header/SearchBar/SearchBar.js
@@ -1,14 +1,31 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import axios from "axios";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch, faXmark } from '@fortawesome/free-solid-svg-icons'
+
 import { setSearchValue } from "../../../redux/searchSlice";
-import { useDispatch } from "react-redux";
-import { authToken } from "../../../key/token";
 import "./SearchBar.css";
 
 function SearchBar() {
   const [searchKey, setSearchKey] = useState("");
   const dispatch = useDispatch();
+
+  // API options
+  const options = {
+    method: "GET",
+    url: "https://spotify23.p.rapidapi.com/search/",
+    params: {
+      q: searchKey,
+      type: "albums",
+      offset: "0",
+      limit: "20",
+    },
+    headers: {
+      'X-RapidAPI-Key': '0171753919msh6e3a8036535490cp1af202jsn7af717784353',
+      'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+    }
+  }
 
   const handleSearch = async (e) => {
     e.preventDefault();
@@ -18,15 +35,8 @@ function SearchBar() {
     }
 
     try {
-      const result = await fetch(`https://api.spotify.com/v1/search?q=${searchKey}&type=album`, {
-        method: "GET",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${authToken}`
-        }
-      })
-      const data = await result.json();
+      const result = await axios.request(options);
+      const data = await result.data;
       dispatch(setSearchValue(data.albums.items));
     } catch (err) {
       alert(err);
@@ -44,7 +54,7 @@ function SearchBar() {
         <input
           type="text"
           className="search-bar__input"
-          placeholder="Search for an album"
+          placeholder="e.g. The Beatles"
           value={searchKey}
           onChange={(e) => setSearchKey(e.target.value)}
           onKeyPress={(e) => e.key === "Enter" && handleSearch(e)}
